4fca9fd4c52fd09eac34f07b5229b8b5d0bc5924
sreyly-long <longsreyly85@gmail.com>
2023-02-07 21:49:52 +0700

    OOP

--- src/Form/Main.java
+++
@@ -1,7 +0,0 @@
-package Form;
-
-public class Main {
-    public static void main(String[] args) {
-        new Form1();
-    }
-}
--- src/Midterm/Main.java
+++
@@ -1,9 +0,0 @@
-package Midterm;
-
-public class Main {
-    public static void main(String[] args) {
-        Account account = new Account(2,"dara",25.00);
-        account.showResult();
-
-    }
-}
